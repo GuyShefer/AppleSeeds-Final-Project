b21b73b7066bfcabcbeea3eb2362963994af2846
Guy <shefer.guy1@gmail.com>
2021-05-07 16:49:27 +0300

    top-sellers container

---
+++ client/src/components/topSellers/TopSellers.component.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './topSellers.style.css';
+import axios from 'axios';
+import ProductCard from '../productCard/ProductCard.component';
+
+const TopSellers = () => {
+
+    const [products, setProducts] = useState([]);
+    const url = 'https://final-project-appleseeds.herokuapp.com';
+
+    useEffect(() => {
+        const getAllTopSellers = async () => {
+            const response = await axios.get(url + '/api/products/bestSeller/all');
+            setProducts(response.data)
+        }
+        getAllTopSellers();
+    }, [])
+
+    return (
+
+        <>
+            {console.log(products)}
+            <div className="top-sellers-grid">
+                {products.map(product => {
+                    return <ProductCard key={product._id} product={product} />
+                })}
+            </div>
+        </>
+    )
+}
+
+export default TopSellers;
--- client/src/pages/main/Main.component.jsx
+++ client/src/pages/main/Main.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TopSellers from '../../components/topSellers/TopSellers.component';
 import './main.style.css';
 
 const Main = () => {
@@ -7,8 +8,14 @@ const Main = () => {
         <>
             <div className="main">
                 <div className="container-image"></div>
-                <div className="best-sellers-section">
-                    <div className="sub-title"><h2>TOP SELLERS</h2></div>
+                <div className="top-sellers-section">
+                    <div className="sub-title">
+                        <h2>TOP SELLERS</h2>
+                    </div>
+
+                    <div className="top-sellers-container-cards">
+                        <TopSellers />
+                    </div>
                 </div>
             </div>
         </>
