2a69786df1e9583a614b3506d3879a8e09791fd9
Guy <shefer.guy1@gmail.com>
2021-05-07 23:11:15 +0300

    footer fixed

--- client/src/pages/main/Main.component.jsx
+++ client/src/pages/main/Main.component.jsx
@@ -17,10 +17,11 @@ const Main = () => {
                         <TopSellers />
                     </div>
 
-                    <div className="welcome-text">
-                        <h3>Welcome to Havaya Jewelry</h3>
-                        <p>We’re here to bring kindness, inspiration, connection, and love straight to your doorstep. Meaning compassion in Hindi, Daya is jewelry with a heart, mission, and story. Featuring handcrafted bohemian pieces for the free spirited, our jewelry combines ancient mythological and spiritual symbols with modern designs.  We aim to be more than just a jewelry company, our mission is simple: spread kindness throughout the world and inspire others to express their individuality.</p>
-                    </div>
+
+                </div>
+                <div className="welcome-text">
+                    <h3>Welcome to Havaya Jewelry</h3>
+                    <p>We’re here to bring kindness, inspiration, connection, and love straight to your doorstep. Meaning compassion in Hindi, Daya is jewelry with a heart, mission, and story. Featuring handcrafted bohemian pieces for the free spirited, our jewelry combines ancient mythological and spiritual symbols with modern designs.  We aim to be more than just a jewelry company, our mission is simple: spread kindness throughout the world and inspire others to express their individuality.</p>
                 </div>
             </div>
         </>
